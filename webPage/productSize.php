b049217561ce5803c52b8f9093eb7d970d8b0183
MadushanSandaru1 <47908590+MadushanSandaru1@users.noreply.github.com>
2020-03-05 11:27:31 +0530

    ajax shoe size control

---
+++ webPage/productSize.php
@@ -0,0 +1,14 @@
+<?php
+
+    require_once('../connection/connection.php');
+
+    session_start();
+
+    $sql = "SELECT * FROM `stock` WHERE `size` = {$_GET['size']}";
+
+    $result = mysqli_query($conn, $sql);
+
+    $rowProId = mysqli_fetch_assoc($result);
+
+    echo $rowProId['available'];
+?>
